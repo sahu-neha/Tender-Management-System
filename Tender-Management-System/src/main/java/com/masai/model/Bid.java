c6e03b53cb43f675cfe1a0eac42665f6236a2933
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-05-04 01:27:03 +0530

    Entity Classes created

--- Tender-Management-System/src/main/java/com/masai/model/Bid.java
+++ Tender-Management-System/src/main/java/com/masai/model/Bid.java
@@ -1,33 +1,39 @@
 package com.masai.model;
 
-import java.math.BigDecimal;
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Bid {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    
-    @ManyToOne
-    @JoinColumn(name = "tenderId")
-    private Tender tender;
-    
-    @ManyToOne
-    @JoinColumn(name = "vendorId")
-    private Vendor vendor;
-    
-    private BigDecimal amount;
-    
-    @Enumerated(EnumType.STRING)
-    private BidStatus status;
-    
-   
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@ManyToOne
+	@JoinColumn(name = "tenderId")
+	private Tender tender;
+
+	@OneToOne
+	@JoinColumn(name = "vendorId")
+	private Vendor vendor;
+
+	private Integer bidAmount;
+
+	private Integer durationInDays;
+
+	@Column(columnDefinition = "default 'PENDING'")
+	private String bidStatus;
+
 }
--- Tender-Management-System/src/main/java/com/masai/model/Dispute.java
+++ Tender-Management-System/src/main/java/com/masai/model/Dispute.java
@@ -5,25 +5,30 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Dispute {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    
-    @ManyToOne
-    @JoinColumn(name = "tenderId")
-    private Tender tender;
-    
-    @ManyToOne
-    @JoinColumn(name = "vendorId")
-    private Vendor vendor;
-    
-    private String message;
-    
-    private boolean resolved;
-    
-   
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@OneToOne
+	@JoinColumn(name = "tenderId")
+	private Tender tender;
+
+	@OneToOne
+	@JoinColumn(name = "vendorId")
+	private Vendor vendor;
+
+	private String message;
+
+	private boolean isResolved;
+
 }
