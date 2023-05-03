cb4f9042b798b2d2450535c7ec954c0648631b1a
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-05-04 04:47:42 +0530

    Vendor Can Update His Password

--- Tender-Management-System/src/main/java/com/masai/repository/VendorRepository.java
+++ Tender-Management-System/src/main/java/com/masai/repository/VendorRepository.java
@@ -1,5 +1,7 @@
 package com.masai.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.masai.model.Vendor;
 
 @Repository
 public interface VendorRepository extends JpaRepository<Vendor, Integer> {
-
+    //This Method For the find Vendor by his UserName
+    Optional<Vendor> findByUsername(String username);
 }
