cbb426bb53c75c3fb1a3447012564f30ac84ae22
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2023-05-04 14:51:10 +0530

    Vendor can see the list of all tenders

--- Tender-Management-System/src/main/java/com/masai/repository/VendorRepository.java
+++ Tender-Management-System/src/main/java/com/masai/repository/VendorRepository.java
@@ -1,14 +1,22 @@
 package com.masai.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.masai.model.Tender;
 import com.masai.model.Vendor;
 
 @Repository
 public interface VendorRepository extends JpaRepository<Vendor, Integer> {
+	
     //This Method For the find Vendor by his UserName
     Optional<Vendor> findByUsername(String username);
+    
+    //This Method Fetch All Tenders
+    @Query("SELECT t FROM Tender t")
+    List<Tender> findAllTenders();
 }
--- Tender-Management-System/src/main/java/com/masai/service/VendorService.java
+++ Tender-Management-System/src/main/java/com/masai/service/VendorService.java
@@ -1,6 +1,9 @@
 package com.masai.service;
 
+import java.util.List;
+
 import com.masai.exception.VendorException;
+import com.masai.model.Tender;
 import com.masai.model.Vendor;
 
 public interface VendorService {
@@ -8,7 +11,10 @@ public interface VendorService {
 	//This Method For Add New Vendor - @Author HoshiyarJyani
 	public Vendor createVendor(Vendor vendor);
 
-	//This Method For Add New Vendor - @Author HoshiyarJyani
+	//This Method For change Password for Vendor - @Author HoshiyarJyani
 	public void updateVendorPassword(String username,String password, String newPassword) throws VendorException;
+
+	//This Method For Get The List Of All Tenders that are Available - @Author HoshiyarJyani
+	public List<Tender> getAllTenders();
 	
 }
