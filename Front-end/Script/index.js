2a46f694884ff41f95568c564fd3f5652dbe2aa6
Neha <nehasahu2227@gmail.com>
2023-05-10 19:26:19 +0530

    landing page created

---
+++ Front-end/Script/index.js
@@ -0,0 +1,7 @@
+function signup() {
+	window.location.href = "/Front-end/VendorMethods/Html/createNewVendor.html";
+}
+
+function login() {
+	window.location.href = "/Front-end/Html/vLogin.html";
+}
